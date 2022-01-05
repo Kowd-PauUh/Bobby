5b446d22ba5ba2e6c8c8eff7ccee57d05b3186d1
Ivan <94719934+Kowd-PauUh@users.noreply.github.com>
2022-01-05 16:32:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ Date 22.12.2021
 
 from os.path import dirname, realpath
 
-TOKEN = '5063051229:AAGqfNAOll-gEQ56VIoBRuZA9vnZV5ETgA0'  # bot token from @BotFather
+TOKEN = 'there should be a token here'  # bot token from @BotFather
 BOT_PATH = dirname(realpath(__file__)).replace('\\', '/')  # .../Bobby
 COSTS_HISTORY_PATH = BOT_PATH + '/data/costs/costs-history.csv'
 USER_CONFIG_PATH = BOT_PATH + '/user_config.ini'
