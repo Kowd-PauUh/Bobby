27df25db2dab9b284cd529a3c16ec85b386e48ee
Ivan <94719934+Kowd-PauUh@users.noreply.github.com>
2022-01-06 16:05:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-"""
-Created by Ivan Danylenko
-Date 22.12.2021
-"""
-
 from os.path import dirname, realpath
 
 TOKEN = 'there should be a token here'  # bot token from @BotFather
+USERS_WITH_ACCESS = [1111111111] # IDs of users with access
+STATUS_CHANNEL = 1111111111 # ID of status channel
+
 BOT_PATH = dirname(realpath(__file__)).replace('\\', '/')  # .../Bobby
 COSTS_HISTORY_PATH = BOT_PATH + '/data/costs/costs-history.csv'
 USER_CONFIG_PATH = BOT_PATH + '/user_config.ini'
