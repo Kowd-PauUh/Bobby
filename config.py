6e68faac11a37566f7d3c3995b3837200d9fe9e0
Ivan <94719934+Kowd-PauUh@users.noreply.github.com>
2022-01-06 17:11:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os.path import dirname, realpath
 
 TOKEN = 'there should be a token here'  # bot token from @BotFather
-USERS_WITH_ACCESS = [1111111111] # IDs of users with access
-STATUS_CHANNEL = 1111111111 # ID of status channel
+USERS_WITH_ACCESS = [] # IDs of users with access
+STATUS_CHANNEL = False # ID of status channel. Enter anything (False for example) if status channel doesn't exist
 
 BOT_PATH = dirname(realpath(__file__)).replace('\\', '/')  # .../Bobby
 COSTS_HISTORY_PATH = BOT_PATH + '/data/costs/costs-history.csv'
