edb913ea0c5702f9369eea71c9901885c8b4ad41
Ivan <94719934+Kowd-PauUh@users.noreply.github.com>
2022-01-25 03:39:16 +0100

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from os.path import dirname, realpath
-
-TOKEN = 'there should be a token here'  # bot token from @BotFather
-USERS_WITH_ACCESS = [] # IDs of users with access
-STATUS_CHANNEL = False # ID of status channel. Enter anything (False for example) if status channel doesn't exist
-
-BOT_PATH = dirname(realpath(__file__)).replace('\\', '/')  # .../Bobby
-COSTS_HISTORY_PATH = BOT_PATH + '/data/costs/costs-history.csv'
-USER_CONFIG_PATH = BOT_PATH + '/user_config.ini'
