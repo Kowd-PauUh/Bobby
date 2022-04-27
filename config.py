6642395e9c5412a5b3d1f2b3b495f66950ef54bd
Ivan <94719934+Kowd-PauUh@users.noreply.github.com>
2022-04-27 19:18:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,15 @@ TOKEN = 'there should be a token here'  # bot token from @BotFather
 BOT_PATH = dirname(realpath(__file__)).replace('\\', '/')  # .../Bobby
 PASSWORDS = {}
 STATUS_CHANNEL = False # ID of status channel. Enter anything (False for example) if status channel doesn't exist
+SECTIONS = ['Diet', 'Costs', 'To-do', 'Settings']
 
-COSTS_HISTORY_PATH = BOT_PATH + '/data/{}/costs/costs-history.csv'
 GENERAL_CONFIG_PATH = BOT_PATH + '/general_config.ini'
-USER_CONFIG_PATH = BOT_PATH + '/data/{}/user_config.ini'
+PRODUCTS_DATABASE_PATH = BOT_PATH + '/data/Databases/cpfc.csv'
+GUIDES_PATH = BOT_PATH + '/data/Guides'
+
+USER_CONFIG_PATH = BOT_PATH + '/data/Users/{}/user_config.ini'
+COSTS_CONFIG_PATH = BOT_PATH + '/data/Users/{}/costs/costs_config.ini'
+DIET_CONFIG_PATH = BOT_PATH + '/data/Users/{}/diet/diet_config.ini'
+TODO_CONFIG_PATH = BOT_PATH + '/data/Users/{}/todo/todo_config.ini'
+
+COSTS_HISTORY_PATH = BOT_PATH + '/data/Users/{}/costs/costs_history.csv'
